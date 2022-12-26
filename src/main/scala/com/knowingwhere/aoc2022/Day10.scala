01231b1e601f50926ca67f696f67caab849704ec
sparkhi <parkhi@gmail.com>
2022-12-26 18:14:34 +0000

    Day 10 Part2 done

--- src/main/scala/com/knowingwhere/aoc2022/Day10.scala
+++ src/main/scala/com/knowingwhere/aoc2022/Day10.scala
@@ -12,6 +12,30 @@ object Day10 extends App {
   val filtered = processedValues._2.filter(thingy => filterList.contains(thingy._1))
   filtered.map(thingy => thingy._1 * thingy._2).sum.pipe(println)
 
+  val drawing = createDrawing(processedValues._2, "")
+  drawing.grouped(40).toList.map(_.pipe(println))
+
+  def getCharToDraw(cycleNumber: Int, spriteLocation: Int) = {
+    val currentDrawPosition = (cycleNumber % 40) - 1
+    val spriteRange = List(spriteLocation - 1, spriteLocation, spriteLocation + 1)
+    if (spriteRange.contains(currentDrawPosition)) {
+      "#"
+    } else {
+      "."
+    }
+  }
+
+  @tailrec
+  def createDrawing(cyclePositionDetails: List[(Int, Int)], drawingAccumulator: String): String = {
+    if (cyclePositionDetails.isEmpty) {
+      drawingAccumulator
+    } else {
+      val instruction = cyclePositionDetails.head
+      val charToDraw = getCharToDraw(instruction._1, instruction._2)
+      createDrawing(cyclePositionDetails.tail, drawingAccumulator + charToDraw)
+    }
+  }
+
 
   def processNoop(processDetails: ProcessDetails, instructionAccumulator: List[(Int, Int)]): (ProcessDetails, List[(Int, Int)]) = {
     val addInstruction = processDetails.completedCycles + 1 -> processDetails.xValue
@@ -32,8 +56,6 @@ object Day10 extends App {
       newProcessDetails -> newInstructionAccumulator
     }
   }
-
-
 
   @tailrec
   def processInstructions(instructions: List[String], processDetails: ProcessDetails, instructionAccumulator: List[(Int, Int)]):(ProcessDetails, List[(Int, Int)]) = {
